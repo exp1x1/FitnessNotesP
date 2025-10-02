e2b9210fd58922d137050c53b3aa5d0aa542c616
vrutikR_exp1x1 <gorvkrtianliya@gmail.com>
2025-10-02 14:05:17 +0530

    removed comment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import { TuiRoot } from "@taiga-ui/core";
+import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
-    imports: [TuiRoot]
+  imports: [TuiRoot],
 })
 export class App {
   protected readonly title = signal('fitnote');
