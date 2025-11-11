7096bba2fe8a0646999f73d6740682c5dc4d88a6
exp1x1 <gorvkrtianliya@gmail.com>
2025-11-12 00:54:25 +0530

    added firebase config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDwcgRCJEZ5ZqE4E6NrVeF-cGiPTFdEmco",
+        authDomain: "fitnotespwa.firebaseapp.com",
+        projectId: "fitnotespwa",
+        storageBucket: "fitnotespwa.firebasestorage.app",
+        messagingSenderId: "272541953950",
+        appId: "1:272541953950:web:d738d891911c3e35ac708f",
+        measurementId: "G-0CLXZVSSXZ"
+    },
+    production: false,
+};
