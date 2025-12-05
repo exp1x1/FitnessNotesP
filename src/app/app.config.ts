57c8bf4d7a612e9ffeffce915ccc462865e97a28
exp1x1 <gorvkrtianliya@gmail.com>
2025-12-05 22:13:02 +0530

    fix eumlator env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,7 +19,7 @@ import {
 } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
